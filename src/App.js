28617e206107440c1b11691241fee2e9cf837f8f
Suhail Kakar <suhail.zahed@gmail.com>
2021-01-21 10:23:22 +0530

    Main Files

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react'
+
+export default class App extends React.Component {
+
+  videoRef = React.createRef();
+  canvasRef = React.createRef();
+
+  styles = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+  };
+
+
+  detectFromVideoFrame = (model, video) => {
+    model.detect(video).then(predictions => {
+      this.showDetections(predictions);
+
+      requestAnimationFrame(() => {
+        this.detectFromVideoFrame(model, video);
+      });
+    }, (error) => {
+      console.log("Couldn't start the webcam")
+      console.error(error)
+    });
+  };
+
+  showDetections = predictions => {
+    const ctx = this.canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    const font = "24px helvetica";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+
+    console.log(predictions)
+
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      const width = prediction.bbox[2];
+      const height = prediction.bbox[3];
+      // Draw the bounding box.
+      ctx.strokeStyle = "#ff2508";
+      ctx.lineWidth = 1;
+      ctx.strokeRect(x, y, width, height);
+      // Draw the label background.
+      ctx.fillStyle = "#ff2508";
+      const textWidth = ctx.measureText(prediction.class).width;
+      const textHeight = parseInt(font, 10);
+      // draw top left rectangle
+      ctx.fillRect(x, y, textWidth + 10, textHeight + 10);
+      // draw bottom left rectangle
+      ctx.fillRect(x, y + height - textHeight, textWidth + 15, textHeight + 10);
+
+      // Draw the text last to ensure it's on top.
+      ctx.fillStyle = "#fff";
+      ctx.fillText(prediction.class, x, y);
+      ctx.fillText(prediction.score.toFixed(2), x, y + height - textHeight);
+    });
+  };
+
+  componentDidMount() {
+    if (navigator.mediaDevices.getUserMedia || navigator.mediaDevices.webkitGetUserMedia) {
+      // define a Promise that'll be used to load the webcam and read its frames
+      const webcamPromise = navigator.mediaDevices
+        .getUserMedia({
+          video: true,
+          audio: false,
+        })
+        .then(stream => {
+          // pass the current frame to the window.stream
+          window.stream = stream;
+          // pass the stream to the videoRef
+          this.videoRef.current.srcObject = stream;
+
+          return new Promise(resolve => {
+            this.videoRef.current.onloadedmetadata = () => {
+              resolve();
+            };
+          });
+        }, (error) => {
+          console.log("Couldn't start the webcam")
+          console.error(error)
+        });
+
+      // define a Promise that'll be used to load the model
+      const loadlModelPromise = cocoSsd.load();
+
+      // resolve all the Promises
+      Promise.all([loadlModelPromise, webcamPromise])
+        .then(values => {
+          this.detectFromVideoFrame(values[0], this.videoRef.current);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+
+
+  render() {
+    return (
+      <div style={{
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        overflow: 'hidden',
+        transform: 'translate(-50%, -50%)',
+      }}>
+
+        <video
+          autoPlay
+          muted
+          playsInline
+          // style={{ border }}
+          ref={this.videoRef}
+          width="700"
+          height="700"
+        />
+        <canvas style={this.styles} ref={this.canvasRef} width="1200" height="1200" />
+      </div>
+    );
+  }
+}
